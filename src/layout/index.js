9221d52644868e72d7f87ed05dc2e47a07f82392
Võ Văn Trường <56033678+vovantruong@users.noreply.github.com>
2022-07-16 02:10:11 +0700

    commit fix css

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import classNames from "classnames/bind";
 import styles from "./DefaultLayout.module.sass";
+import { useMediaQuery } from "react-responsive";
 
 const cx = classNames.bind(styles);
 
 const DefaultLayout = () => {
+  const isMobile = useMediaQuery({ query: `(max-width: 739px)` });
   return (
     <div className={cx("wrapper")}>
       <span className={cx("bg-attachment")}>
-        <h1>GAVIN</h1>
+        <h1>{isMobile ? "GAVIN" : <marquee>GAVIN</marquee>}</h1>
       </span>
     </div>
   );
